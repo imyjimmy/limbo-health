e6466874891d37bae36055ba84df21f48cea56a8
imyjimmy <imyjimmy@gmail.com>
2026-02-07 19:45:21 -0600

    db settings for Railway

--- apps/scheduler-api/config/database.js
+++ apps/scheduler-api/config/database.js
@@ -6,9 +6,10 @@ const getMySQLHost = () => {
 
 const pool = mysql.createPool({
   host: getMySQLHost(),
-  user: 'user',
-  password: 'password',
-  database: 'limbo_health',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'limbo_health',
+  port: parseInt(process.env.DB_PORT || '3306'),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
